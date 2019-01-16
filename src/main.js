8a0198985552f07acdadd76712d0cc19d4efc17e
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2019-01-16 22:32:51 +0530

    Introduced show() in main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ const getJar = document => document.getElementById("jar_1");
 
 const hide = function() {
   getJar(document).style.visibility = "hidden";
-  setTimeout(() => {
-    getJar(document).style.visibility = "visible";
-  }, 1000);
+  setTimeout(show, 1000);
+};
+
+const show = function() {
+  getJar(document).style.visibility = "visible";
 };
 
 const initialize = function() {
