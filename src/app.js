264fee0c170a66f2a86dc578d4915aa2a0c1b928
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2019-01-16 21:54:02 +0530

    Modified app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,29 @@
 const { readFile } = require("fs");
 
 const getRequest = function(url) {
-  if (url == "/") {
-    return "./index.html";
-  }
+  if (url == "/") return "./index.html";
   return "." + url;
+};
+
+const sendResponse = function(res, content, status) {
+  res.statusCode = status;
+  res.write(content);
+  res.end();
+};
+
+const handleRequest = function(request, res) {
+  readFile(request, function(err, content) {
+    if (err) {
+      sendResponse(res, "not found", 404);
+      return;
+    }
+    sendResponse(res, content, 200);
+  });
 };
 
 const app = (req, res) => {
   let request = getRequest(req.url);
-  readFile(request, function(err, content) {
-    if (!err) {
-      res.write(content);
-      res.end();
-      return;
-    }
-    res.statusCode = 404;
-    res.end();
-  });
+  handleRequest(request, res);
 };
 
 module.exports = app;
