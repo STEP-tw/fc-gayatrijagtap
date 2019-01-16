3189ed60b8f346938d13a8bef9c110bc797d6118
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2019-01-16 15:31:42 +0530

    Handled favicon error

--- src/app.js
+++ src/app.js
@@ -1,28 +1,23 @@
 const { readFile } = require("fs");
 
+const getRequest = function(url) {
+  if (url == "/") {
+    return "./index.html";
+  }
+  return "." + url;
+};
+
 const app = (req, res) => {
-  let request = "index.html";
-  if (req.url == "/style_sheet/style.css") {
-    request = "./style_sheet/style.css";
-  }
-
-  if (req.url == "/fs-gayatrijagtap/media/freshorigins.jpg") {
-    request = "./media/freshorigins.jpg";
-  }
-
-  if (req.url == "/fs-gayatrijagtap/media/animated-flower-image-0021.gif") {
-    request = "./media/animated-flower-image-0021.gif";
-  }
-
-  if (req.url == "/src/main.js") {
-    request = "./src/main.js";
-  }
-
+  let request = getRequest(req.url);
   readFile(request, function(err, content) {
-    res.write(content);
+    if (!err) {
+      res.write(content);
+      res.end();
+      return;
+    }
+    res.statusCode = 404;
     res.end();
   });
 };
-// Export a function that can act as a handler
 
 module.exports = app;
