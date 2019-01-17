b90b6f08c4531f1b0e0381bf968affd818fb3aab
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2019-01-17 13:25:12 +0530

    Implemented Sheeghra

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
-const PAGE_NOT_FOUND = "Page Not Found";
+const Sheeghra = require("../sheeghra");
+const app = new Sheeghra();
+
+const NOT_FOUND = "Page Not Found";
 const { readFile } = require("fs");
 
 const getRequest = function(url) {
   if (url == "/") return "./public_html/index.html";
   return "./public_html" + url;
+};
+
+const readBody = (req, res, next) => {
+  let content = "";
+  req.on("data", chunk => (content += chunk));
+  req.on("end", () => {
+    req.body = content;
+    next();
+  });
 };
 
 const sendResponse = function(res, content, status) {
@@ -12,19 +24,29 @@ const sendResponse = function(res, content, status) {
   res.end();
 };
 
-const handleRequest = function(request, res) {
+const handleRequest = function(req, res) {
+  let request = getRequest(req.url);
   readFile(request, function(err, content) {
     if (err) {
-      sendResponse(res, PAGE_NOT_FOUND, 404);
+      sendResponse(res, NOT_FOUND, 404);
       return;
     }
     sendResponse(res, content, 200);
   });
 };
 
-const app = (req, res) => {
-  let request = getRequest(req.url);
-  handleRequest(request, res);
+const getFormData = function(req, res) {
+  let formData = req.body.match(/name\=(.*)\&comment=(.*)/);
+  let name = formData[1];
+  let comment = formData[2];
+  let date = new Date();
+  let response = "name:" + name + "\ncomment:" + comment + "\ndate:" + date;
+  res.write(response);
+  res.end();
 };
 
-module.exports = app;
+app.use(readBody);
+app.post("/submitted", getFormData);
+app.use(handleRequest);
+
+module.exports = app.handleRequest.bind(app);
